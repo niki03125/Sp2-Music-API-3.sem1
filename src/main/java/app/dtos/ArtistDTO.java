7c53eca376768d8689ad6881aadcf36a0ed791c3
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-10-22 15:20:02 +0200

    artistdto and service

--- src/main/java/app/dtos/ArtistDTO.java
+++ src/main/java/app/dtos/ArtistDTO.java
@@ -20,43 +20,7 @@ import app.entities.Artist;
 public class ArtistDTO {
     private Integer id;
     private String name;
-    private List<SongDTO> songs;
-    private List<AlbumDTO> albums;
+    private Set<SongDTO> songs;
+    private Set<AlbumDTO> albums;
 
-    public ArtistDTO toDTO(Artist artist, boolean includeSongs, boolean includeAlbums){
-        if(artist == null) return null;
-
-        var builder = ArtistDTO.builder()
-                .id(artist.getId())
-                .name(artist.getName());
-
-        if(includeSongs && artist.getSongs() != null){
-            builder.songs(
-                    artist.getSongs().stream()
-                            .map(SongService::toDTO)
-                            .collect(Collectors.toList())
-
-            );
-        }
-
-        if(includeAlbums && artist.getAlbums() != null){
-            builder.albums(
-                    artist.getAlbums().stream()
-                            .map(AlbumDTO::fromEntity)
-                            .collect(Collectors.toList())
-            );
-        }
-        return builder.build();
-    }
-
-    public Artist toEntity(){
-        Artist artist = new Artist();
-
-        if(this.id != null){
-            artist.setId(this.id);
-        }
-
-        artist.setName(this.name);
-        return artist;
-    }
 }
