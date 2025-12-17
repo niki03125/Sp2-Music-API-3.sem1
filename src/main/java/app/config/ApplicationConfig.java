716837fd7982ef9766bd5b583a06a09fdc7e1ec1
niki03125 <Nikita.po@hotmail.com>
2025-12-17 12:56:10 +0100

    added cors to ApplicationConfig

--- src/main/java/app/config/ApplicationConfig.java
+++ src/main/java/app/config/ApplicationConfig.java
@@ -3,11 +3,12 @@ package app.config;
 import app.routes.Routes;
 import io.javalin.Javalin;
 import io.javalin.config.JavalinConfig;
+import io.javalin.http.Context;
 
 public class ApplicationConfig {
     private static Routes routes = new Routes();
 
-    public static void configuration(JavalinConfig config){
+    public static void configuration(JavalinConfig config) {
         config.showJavalinBanner = false;
         config.bundledPlugins.enableRouteOverview("/routes");
         config.router.contextPath = "/api/v1"; // base path for all endpoints
@@ -17,6 +18,8 @@ public class ApplicationConfig {
     public static Javalin startServer(int port) {
         routes = new Routes();
         var app = Javalin.create(ApplicationConfig::configuration);
+        app.before(ApplicationConfig::corsHeaders);
+        app.options("/*", ApplicationConfig::corsHeadersOptions);
         app.start(port);
         return app;
     }
@@ -24,4 +27,20 @@ public class ApplicationConfig {
     public static void stopServer(Javalin app) {
         app.stop();
     }
+
+    private static void corsHeaders(Context ctx) {
+        ctx.header("Access-Control-Allow-Origin", "*");
+        ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        ctx.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        ctx.header("Access-Control-Allow-Credentials", "true");
+    }
+
+    private static void corsHeadersOptions(Context ctx) {
+        ctx.header("Access-Control-Allow-Origin", "*");
+        ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        ctx.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        ctx.header("Access-Control-Allow-Credentials", "true");
+        ctx.status(204);
+    }
+
 }
